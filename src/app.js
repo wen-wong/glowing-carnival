0d639433fb65b255bd9d03b6b6157a60e925c437
wen-wong <wen.wong@mail.mcgill.ca>
2023-04-27 19:30:44 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+const config = require("./config/config");
+
+const logger = require("pino")();
+const app = express();
+
+mongoose.set("strictQuery", false);
+mongoose
+	.connect(config.mongo.url, { retryWrites: true, w: "majority" })
+	.then(() => {
+		logger.info("Connected to MongoDB.");
+		startServer();
+	});
+
+const startServer = () => {
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
+
+	// Handle Incoming Requests
+	app.use((req, res, next) => {
+		logger.info(
+			`Incoming -> Method [${req.method}] - Uri: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+		);
+		res.on("finish", () => {
+			logger.info(
+				`Incoming -> Method [${req.method}] - Uri: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${req.statusCode}]`)
+		});
+		next();
+	});
+
+	// Healthcheck Endpoint
+	app.get("/", (_req, res, _next) =>
+		res.status(200).json({ message: "Healthcheck Achieved." })
+	);
+
+	// Handle Non-existing Routes
+	app.use((_req, res, _next) => {
+		const error = new Error("not found");
+		logger.error(error);
+		return res.status(404).json({ message: error.message });
+	});
+
+	http.createServer(app).listen(config.server.port, () =>
+		logger.info(`Server is running on port ${config.server.port}.`)
+	);
+}
+
---
+++ src/config/config.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+
+const MONGO_URL = `mongodb+srv//${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.rujkjfu.mongodb.net/test`;
+const SERVER_PORT = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 1337;
+
+const config = {
+	mongo: {
+		url: MONGO_URL
+	},
+	server: {
+		port: SERVER_PORT
+	}
+};
+
+module.exports = config;
