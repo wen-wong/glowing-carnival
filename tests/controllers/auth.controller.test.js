5554ebc333f47b97b44934161002c40fefd86550
ahuang28 <amyhuang0605@gmail.com>
2023-05-07 12:46:02 -0400

    refreshtoken

--- tests/controllers/auth.controller.test.js
+++ tests/controllers/auth.controller.test.js
@@ -1,5 +1,5 @@
 const User = require("../../src/models/user.model")
-const { register } = require("../../src/controllers/auth.controller");
+const { register, refreshToken } = require("../../src/controllers/auth.controller");
 const next = require("../../src/for_next/services");
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
@@ -143,3 +143,72 @@ describe('register', () => {
 * test case for if session.commitTransaction()
 * test case for error
 * */
+
+
+describe('refreshToken endpoint', () => {
+    let user;
+    let refreshToken;
+    const req = { body: { refreshToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDU0MDg1NzE3MGMyOTE5N2U0MzZhMDUiLCJpYXQiOjE2ODMyMzIwODEsImV4cCI6MTY4MzgzNjg4MX0.CV9KXNob6VSU82oUe3A9vGLAS6b8uTpVJ7Ozb8CpXqU' } };
+    const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+
+    beforeAll(async () => {
+        user = new User({
+            email: 'test@example.com',
+            password: 'testpassword',
+            phone: '6176103635'
+        });
+        await user.save();
+        refreshedToken = user.generateRefreshToken;
+    });
+
+    afterAll(async () => {
+        await User.deleteMany({});
+    });
+
+    test('should return a new access token and refresh token', async () => {
+        //const response = await register.refreshToken(user);
+            //.post('/refresh-token')
+            //.send({ refreshToken })
+            //.expect(200);
+        await refreshToken(req, res);
+       // expect(response.body).toHaveProperty('accessToken');
+        //expect(response.body).toHaveProperty('refreshToken');
+        expect(res.status).toHaveBeenCalledWith(200);
+    });
+
+    // test('should update the user with the new refresh token', async () => {
+    //     const response = await request(app)
+    //         .post('/refresh-token')
+    //         .send({ refreshToken })
+    //         .expect(200);
+    //
+    //     const updatedUser = await User.findById(user._id);
+    //     expect(updatedUser.refreshTokens).toContain(response.body.refreshToken);
+    //     expect(updatedUser.refreshTokens).not.toContain(refreshToken);
+    // });
+    //
+    // test('should return 401 if the refresh token is invalid', async () => {
+    //     const response = await request(app)
+    //         .post('/refresh-token')
+    //         .send({ refreshToken: 'invalid-token' })
+    //         .expect(401);
+    //
+    //     expect(response.body).toEqual({ message: 'Invalid refresh token' });
+    // });
+    //
+    // test('should return 500 if there is an internal server error', async () => {
+    //     jest.spyOn(User, 'findById').mockImplementation(() => {
+    //         throw new Error('Internal server error');
+    //     });
+    //
+    //     const response = await request(app)
+    //         .post('/refresh-token')
+    //         .send({ refreshToken })
+    //         .expect(500);
+    //
+    //     expect(response.body).toEqual({ message: 'Internal server error' });
+    //
+    //     User.findById.mockRestore();
+    // });
+});
