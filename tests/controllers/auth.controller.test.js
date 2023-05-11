75ea050cf9a5b2e6ef66839807a67851f57bbb9b
Amy Huang <98197511+ahuang28@users.noreply.github.com>
2023-05-11 12:44:50 -0400

    i think i did everything about validation? (#5)

--- tests/controllers/auth.controller.test.js
+++ tests/controllers/auth.controller.test.js
@@ -4,8 +4,9 @@ const { MongoMemoryServer } = require("mongodb-memory-server");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { register, login, refreshToken } = require("../../src/controllers/auth.controller");
-let mongoServer;
 const validateUser = require("../../src/middlewares/validateUser");
+
+let mongoServer;
 
 beforeAll(async () => {
 	mongoServer = new MongoMemoryServer();
@@ -19,115 +20,101 @@ afterAll(async () => {
 	await mongoServer.stop();
 });
 
-describe("register", () => {
-	const req = { body: { name: "John Doe", email: "john@example.com", password: "password123" } };
-	const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-	const next = jest.fn();
+describe('register', () => {
+    const req = { body: { name: 'John Doe', email: 'john@example.com', password: 'Password@123' } };
+    const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+    const next = jest.fn();
 
-	beforeEach(() => {
-		User.mockClear;
-		res.status.mockClear();
-		res.json.mockClear();
-		next.mockClear();
-	});
+    beforeEach(() => {
+        User.mockClear;
+        res.status.mockClear();
+        res.json.mockClear();
+        next.mockClear();
+    });
 
-	it("should create a new user and generate a token", async () => {
-		const user = new User({
-			name: "John Doe",
-			email: "john@example.com",
-			password: "password123"
-		});
-		User.mockReturnValueOnce;
-		user.save = jest.fn();
-		jest.spyOn(user, "generateAuthToken").mockResolvedValue(String);
-		await register(req, res, next);
-		expect(res.status).toHaveBeenCalledWith(201);
-		expect(res.json).toHaveBeenCalledWith({
-			user: expect.any(Object),
-			token: expect.any(Object)
-		});
-	});
+    it('should create a new user and generate a token', async () => {
+        const user = new User({ name: 'John Doe', email: 'john@example.com', password: 'Password@123'});
+        User.mockReturnValueOnce;
+        user.save = jest.fn();
+        jest.spyOn(user, "generateAuthToken").mockResolvedValue(Object);
 
-	it("should log an error if an error occurs", async () => {
-		const error = new Error("bruh");
-		const user = new User({
-			name: "John Doe",
-			email: "john@example.com",
-			password: "password123"
-		});
-		User.mockImplementationOnce;
-		jest.spyOn(user, "save").mockResolvedValue(Error);
-		await register(req, res, next);
+        await register(req, res, next);
 
-		expect(res.status).not.toHaveBeenCalled();
-		expect(res.json).not.toHaveBeenCalled();
-	});
-});
+        expect(res.status).toHaveBeenCalledWith(201);
+        expect(res.json).toHaveBeenCalledWith({ user: expect.any(Object), token: expect.any(Object) });
 
-describe("refreshToken endpoint", () => {
-	let refreshedToken;
+    });
 
-	req = {
-		body: { name: "John 2", email: "john2@example.com", password: "password123", phone: "617" }
-	};
-	const res = { status: jest.fn().mockReturnThis(), json: jest.fn(), send: jest.fn() };
-	const next = jest.fn();
+    it('should log an error if an error occurs', async () => {
+        const error = new Error('bruh');
+        const user = new User({ name: 'John Doe', email: 'john@example.com', password: 'Password@123'});
+        User.mockImplementationOnce;
+      
+        jest.spyOn(user, "save").mockResolvedValue(Error);
+        await register(req, res, next);
 
-	beforeAll(async () => {
-		const user = new User({
-			name: "John 2",
-			email: "john2@example.com",
-			password: "password123",
-			phone: "617"
-		});
+        expect(res.status).not.toHaveBeenCalled();
+        expect(res.json).not.toHaveBeenCalled();
 
-		await register(req, res, next);
-		await user.save();
-		refreshedToken = user.generateAuthToken();
+    });
 
-		User.mockClear;
-		res.status.mockClear();
-		res.send.mockClear();
-	});
 
-	test("should return a new access token and refresh token", async () => {
-		req = { body: { refreshToken: refreshedToken.refreshToken } };
-		await refreshToken(req, res);
-		expect(res.send).toHaveBeenCalledWith({
-			accessToken: expect.any(Object),
-			refreshToken: expect.any(String)
-		});
-	});
+describe('refreshToken endpoint', () => {
+    let refreshedToken;
 
-	it("401 if no user found (invalid refresh token)", async () => {
-		jest.spyOn(jwt, "verify").mockResolvedValue(String);
-		jest.spyOn(User, "findById").mockResolvedValue();
-		await refreshToken(req, res);
-		expect(res.status).toHaveBeenCalledWith(401);
-		expect(res.send).toHaveBeenCalledWith({ message: "Invalid refresh token" });
-	});
+    req = {body: {name: 'John 2', email: 'john2@example.com', password: 'Password@123', phone: '617-610-3635'}};
+    const res = {status: jest.fn().mockReturnThis(), json: jest.fn(), send: jest.fn()};
+    const next = jest.fn();
 
-	it("500 for other errors", async () => {
-		const t_user = new User({
-			email: "test@example.com",
-			password: "testpassword",
-			phone: "6176103635"
-		});
-		jest.spyOn(jwt, "verify").mockResolvedValue(String);
-		jest.spyOn(User, "findById").mockResolvedValue(t_user);
-		jest.spyOn(t_user, "generateAuthToken").mockResolvedValue(String);
-		jest.spyOn(jwt, "sign").mockResolvedValue(Error);
-		await refreshToken(req, res);
-		expect(res.status).toHaveBeenCalledWith(500);
-		expect(res.send).toHaveBeenCalledWith({ message: "Internal server error" });
-	});
+    beforeAll(async () => {
+        const user = new User({name: 'John 2', email: 'john2@example.com', password: 'Password@123', phone: '617-610-3635'});
+
+        await register(req, res, next);
+        await user.save();
+        refreshedToken = user.generateAuthToken();
+
+        User.mockClear;
+        res.status.mockClear();
+        res.send.mockClear();
+    });
+
+    test('should return a new access token and refresh token', async () => {
+
+        req = {body: {refreshToken: refreshedToken.refreshToken}};
+        await refreshToken(req, res);
+        expect(res.send).toHaveBeenCalledWith({accessToken: expect.any(Object), refreshToken: expect.any(String)});
+
+    });
+
+    it("401 if no user found (invalid refresh token)", async () => {
+        jest.spyOn(jwt, "verify").mockResolvedValue(String);
+        jest.spyOn(User, "findById").mockResolvedValue();
+        await refreshToken(req, res);
+        expect(res.status).toHaveBeenCalledWith(401);
+        expect(res.send).toHaveBeenCalledWith({message: "Invalid refresh token"});
+    });
+
+    it("500 for other errors", async () => {
+        const t_user = new User({
+            email: "test@example.com",
+            password: "test$4Password",
+            phone: "617-610-3635"
+        });
+        jest.spyOn(jwt, "verify").mockResolvedValue(String);
+        jest.spyOn(User, "findById").mockResolvedValue(t_user);
+        jest.spyOn(t_user, "generateAuthToken").mockResolvedValue(String);
+        jest.spyOn(jwt, "sign").mockResolvedValue(Error);
+        await refreshToken(req, res);
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.send).toHaveBeenCalledWith({message: "Internal server error"});
+    });
 });
 
 describe("login function", () => {
 	let req, res, next;
 
 	beforeEach(() => {
-		req = { body: { email: "amen@bob.com", password: "123" } };
+		req = { body: { email: "amen@bob.com", password: "Password@123" } };
 		res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 		next = jest.fn();
 	});
@@ -137,7 +124,7 @@ describe("login function", () => {
 	});
 
 	it("return token for valid email & password", async () => {
-		const f_user = new User({ email: "amen@bob.com", password: "123" });
+		const f_user = new User({ email: "amen@bob.com", password: "Password@123" });
 
 		jest.spyOn(User, "findOne").mockResolvedValue(f_user);
 		jest.spyOn(bcrypt, "compare").mockResolvedValue(true);
@@ -149,7 +136,7 @@ describe("login function", () => {
 		);
 		await login(req, res, next);
 		expect(User.findOne).toHaveBeenCalledWith({ email: "amen@bob.com" });
-		expect(bcrypt.compare).toHaveBeenCalledWith("123", f_user.password);
+		expect(bcrypt.compare).toHaveBeenCalledWith("Password@123", f_user.password);
 		expect(f_user.generateAuthToken).toHaveBeenCalled();
 		expect(res.status).toHaveBeenCalledWith(200);
 		expect(res.json).toHaveBeenCalledWith({
@@ -162,7 +149,7 @@ describe("login function", () => {
 	});
 
 	it("401 if email incorrect", async () => {
-		const f_user = new User({ email: "amen@bob.com", password: "123" });
+		const f_user = new User({ email: "amen@bob.com", password: "Password@123" });
 		jest.spyOn(User, "findOne").mockResolvedValue(null);
 		jest.spyOn(bcrypt, "compare");
 		jest.spyOn(f_user, "generateAuthToken");
@@ -176,13 +163,13 @@ describe("login function", () => {
 	});
 
 	it("401 if password incorrect", async () => {
-		const f_user = new User({ email: "amen@bob.com", password: "123" });
+		const f_user = new User({ email: "amen@bob.com", password: "Password@123" });
 		jest.spyOn(User, "findOne").mockResolvedValue(f_user);
 		jest.spyOn(bcrypt, "compare").mockResolvedValue(false);
 		jest.spyOn(f_user, "generateAuthToken");
 		await login(req, res, next);
 		expect(User.findOne).toHaveBeenCalledWith({ email: "amen@bob.com" });
-		expect(bcrypt.compare).toHaveBeenCalledWith("123", f_user.password);
+		expect(bcrypt.compare).toHaveBeenCalledWith("Password@123", f_user.password);
 		expect(res.status).toHaveBeenCalledWith(401);
 		expect(res.json).toHaveBeenCalledWith({ message: "Invalid email or password" });
 		expect(f_user.generateAuthToken).not.toHaveBeenCalled();
@@ -190,7 +177,7 @@ describe("login function", () => {
 	});
 
 	it("catch any other error", async () => {
-		const f_user = new User({ email: "amen@bob.com", password: "123" });
+		const f_user = new User({ email: "amen@bob.com", password: "Password@123" });
 		jest.spyOn(User, "findOne").rejects;
 		jest.spyOn(bcrypt, "compare");
 		jest.spyOn(f_user, "generateAuthToken");
--- tests/models/user.model.test.js
+++ tests/models/user.model.test.js
@@ -17,14 +17,192 @@ afterAll(async () => {
 });
 
 describe("User Schema", () => {
+	//WORKING CASE TESTS
 	test("should insert a User document correctly", async () => {
 		const user = new User({
 			email: "elmo@mail.ca",
-			password: "elmo",
+			password: "Elmo$999",
 			name: "Elmo",
 			phone: "123-123-1234",
-			role: "user"
+			role: "user",
+			refreshTokens: ["1"]
 		});
 		await expect(user.save()).resolves.toBeDefined();
 	});
+
+	test("user can be retrieved from database and it matches info", async () => {
+		const user = new User({
+			email: "elmo2@mail.ca",
+			password: "Elmo$999",
+			name: "Elmo",
+		});
+		await user.save();
+
+		const retrievedUser = await User.findById(user._id);
+		expect(retrievedUser.email).toEqual(user.email);
+		expect(retrievedUser.name).toEqual(user.name);
+
+	});
+
+	test("user can be updated with valid info", async () => {
+		const user = new User({
+			email: "elmo3@mail.ca",
+			password: "Elmo$999",
+			name: "Elmo",
+			phone: "123-123-1200",
+			role: "user"
+		});
+
+		await user.save();
+
+		const updatedUserData = {
+			email: 'updateduser@example.com',
+			phone: '555-555-5555',
+			name: 'elmo 2.0'
+		};
+
+		const updatedUser = await User.findByIdAndUpdate(user._id, updatedUserData, { new : true });
+		expect(updatedUser.email).toEqual(updatedUserData.email);
+		expect(updatedUser.phone).toEqual(updatedUserData.phone);
+		expect(updatedUser.name).toEqual(updatedUserData.name);
+
+	});
+
+
+	//ERROR CASE TESTS
+	test("error thrown when user created w/o email", async () => {
+		expect.assertions(1);
+		const user = new User({
+			password: "fRog$1114"
+		});
+
+		try {
+			await user.save();
+		} catch(err) {
+			expect(err).toBeTruthy();
+		}
+
+	});
+
+	test("invalid email format", async () => {
+		expect.assertions(1);
+		const user = new User({
+			email: "frog",
+			password: "fRog$1114"
+		});
+
+		try {
+			await user.save();
+		} catch(err) {
+			expect(err).toBeTruthy();
+		}
+
+	});
+
+	test("duplicate email", async () => {
+		expect.assertions(1);
+		const user = new User({
+			email: "bigfrog@mail.com",
+			password: "fRog$1114",
+			phone: "111-111-1111"
+		});
+		await user.save();
+
+		const dup_user = new User({
+			email: "bigfrog@mail.com",
+			password: "Password@123"
+		});
+
+		try {
+			await dup_user.save();
+		} catch(err) {
+			expect(err).toBeTruthy();
+		}
+
+	});
+
+	test("no password", async () => {
+		expect.assertions(1);
+		const user = new User({
+			email: "frog@mail.com"
+		});
+
+		try {
+			await user.save();
+		} catch(err) {
+			expect(err).toBeTruthy();
+		}
+
+	});
+
+	test("invalid password format (weak password)", async () => {
+		expect.assertions(1);
+		const user = new User({
+			email: "frog@mail.com",
+			password: "frog"
+		});
+
+		try {
+			await user.save();
+		} catch(err) {
+			expect(err).toBeTruthy();
+		}
+
+	});
+
+	test("invalid phone number", async () => {
+		expect.assertions(1);
+		const user = new User({
+			email: "frog@mail.com",
+			password: "fRog$1114",
+			phone: "617"
+		});
+
+		try {
+			await user.save();
+		} catch(err) {
+			expect(err).toBeTruthy();
+		}
+
+	});
+
+	test("error thrown when trying to create a user with duplicate phone number", async () => {
+		expect.assertions(1);
+		const user = new User({
+			email: "frog@mail.com",
+			password: "fRog$1114",
+			phone: "617-610-3635"
+		});
+		await user.save();
+
+		const dup_user = new User({
+			email: "otherfrog@mail.com",
+			password: "Password@123",
+			phone: "617-610-3635"
+		});
+
+		try {
+			await dup_user.save();
+		} catch(err) {
+			expect(err).toBeTruthy();
+		}
+
+	});
+
+	test("error thrown when trying to create a user with a role that is not in roles", async () => {
+		expect.assertions(1);
+		const user = new User({
+			email: "frog@mail.com",
+			password: "Password@123",
+			role: "not a valid role"
+		});
+
+		try {
+			await user.save();
+		} catch(err) {
+			expect(err).toBeTruthy();
+		}
+
+	});
+
 });
